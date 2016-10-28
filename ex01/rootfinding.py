6c89821a8d85fd76cf43b8cbc072a9eb9a352e2c
Thomas Rometsch <thomas.rometsch@gmail.com>
2016-10-28 21:41:37 +0200

    try sympys mpmath to set precision manually

--- ex01/rootfinding.py
+++ ex01/rootfinding.py
@@ -5,17 +5,19 @@ Thomas Rometsch
 Solution to Excercise Sheet 1
 """
 
-import numpy as np;
+from sympy import mpmath as mp;
 
-N_max_iteration = 20;
+N_max_iteration = 1000;
+
+mp.dps = 40;
 
 # Define function
 def f(x):
-    return np.exp(np.sqrt(5)*x) - 13.5*np.cos(0.1*x) * 25*x**4;
+    return mp.exp(mp.sqrt(5)*x) - 13.5*mp.cos(0.1*x) * 25*mp.power(x,4);
 
 # Derivative of function f
 def fp(x):
-    return np.sqrt(5)*np.exp(np.sqrt(5)*x) + 1.35*np.sin(0.1*x) + 100*x**3;
+    return mp.sqrt(5)*mp.exp(mp.sqrt(5)*x) + 1.35*mp.sin(0.1*x) + 100*mp.power(x,3);
 
 # Function to call a given method untill a certain accuracy is reached.
 # @param:
@@ -38,7 +40,7 @@ def find_root(eps,a,b,method,verbosity=2):
             print("{0:4d} \t {1:.2e} \t {2:.14e} \t {3:.4e} \t {4:.4e} \t {5:.2e}".format(Niter,delta,r,a,b,f(r))); 
         if delta<=eps:
             break;
-    print("# After {0} iterations found root r = {1:.14e} with delta = {2:.3e}.".format(Niter,r,delta));
+    print("# After {0:d} iterations found root r = {1:.14e} with delta = {2:.3e}.".format(Niter,float(r),float(delta)));
 
 
 # implement bisection method
@@ -62,23 +64,23 @@ def lin_interpol(a,b):
     else:
         a=c;
     return c,a,b,f(c);
-    
+
 # implement newtons method
 # here a = x_n
 def newton(a,b):
     r = a - f(a)/fp(a);
     if f(r) == 0:
         return r,r,r,0;
-    return r,r,0,np.abs(r-a);
+    return r,r,0,abs(r-a);
 
 
 def main():
-    a = 0;
-    b = 1;
-    eps = 10**-14;
+    a = mp.mpf(0);
+    b = mp.mpf(1);
+    eps = mp.mpf(10**-18);
     find_root(eps,a,b,bisection,0);
     find_root(eps,a,b,lin_interpol,0);
-    find_root(eps,a,b,newton,2);
+    find_root(eps,a,b,newton,0);
 
 if __name__=="__main__":
     main();
